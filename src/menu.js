e9e729677c4465638f1db9379bea22eab3f4372c
alabador <austinlabador@gmail.com>
2022-07-01 02:42:22 -0700

    Add menu item to menu

--- src/menu.js
+++ src/menu.js
@@ -11,12 +11,16 @@ const lechonKawali = new MenuItem('Lechon Kawali',
     'Pork belly slabs deep-fried, served in cubes.',
     'https://images.unsplash.com/photo-1625477811233-044633d10dd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
 );
+const bbq = new MenuItem('BBQ', 
+    'Various meats grilled on a stick',
+    'https://images.unsplash.com/photo-1593870682262-8c9f6a9bb225?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+);
 
 function menu() {
     const menuDiv = document.createElement('div');
     menuDiv.classList.add('menu-div');
 
-    menuDiv.append(menuHeader(), menuItem(lechonKawali));
+    menuDiv.append(menuHeader(), menuItem(lechonKawali), menuItem(bbq));
     return menuDiv;
 }
 
